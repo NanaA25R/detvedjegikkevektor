6239137ceb1c7b5874e04c74f14d6da9dbbb7ee1
AyaS25R <at25asay@edu.aarhustech.dk>
2026-06-08 09:16:33 +0200

    har næsten lavet hele koden til def vektor2D længde
    mangler bare lidt labels og at placere alt rigtigt

--- ui.py
+++ ui.py
@@ -1,3 +1,28 @@
-print("hej")
-print("no")
-print("Yeah!!")
+import tkinter
+
+
+vinduet=tkinter.Tk()
+vinduet.geometry('1000x1000')
+
+def Vektor2DLængde(x1,y1,x2,y2):
+    
+    længde1 =float(x1inputfelt.get())
+    længde2=float(x2inputfelt.get())
+
+
+
+x1inputfelt=tkinter.Entry(vinduet)
+x1inputfelt.place(x=200,y=300)
+
+x2inputfelt=tkinter.Entry(vinduet)
+x2inputfelt.place(x=100,y=100)
+
+xlabel=tkinter.Label(vinduet)
+xlabel.place(x=500,y=500)
+xlabel.config(text=længde1)
+
+
+
+
+
+vinduet.mainloop()
