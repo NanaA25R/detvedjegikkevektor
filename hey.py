f0c8f16ebf4324b7fb74dd916bca6f09bb8e3b40
NanaA25R <nanafynboagersbaek@gmail.com>
2026-06-11 08:36:02 +0200

    jj

--- hey.py
+++ hey.py
@@ -1,4 +1,95 @@
-print ("hey")
-print("hey")¨
-print("hey")
-print("hey")
+import math
+
+def Vektor2DLængde(x1,y1,x2,y2):
+    x1 = float(x1)
+    y1 = float(y1)
+    x2 = float(x2)
+    y2 = float(y2)
+
+    længde = math.sqrt((x1**2) + (y1**2))
+    længde2 = math.sqrt(x2**2 + y2**2)
+    print('længden af vektor1=')
+    print(længde)
+    print('længden af vektor2=')
+    print(længde2)
+
+def  Vektor2Dminus(x1,y1,x2,y2):
+    minus1X2 = (x1-x2)
+    minus1Y2 = (y1-y2)
+
+    minus2X1 = (x2-x1)
+    minus2Y1 = (y2-y1)
+    print('minusverdien af v1-v2 er')
+    print('x=')
+    print(minus1X2)
+    print('Y=')
+    print(minus1Y2)
+
+    print("minusværdien af v2-v1 er")
+    print('x=')
+    print(minus2X1)
+    print('Y=')
+    print(minus2Y1)
+
+def vektor2Dplus(x1,y1,x2,y2):
+    plusX = (x1+x2)
+    plusY = (y1+y2)
+    print('x=')
+    print(plusX)
+    print('Y=')
+    print(plusY)
+
+def Vektor2DlægdeAFplus(x1,x2,y1,y2):
+    x1 = float(x1)
+    y1 = float(y1)
+    x2 = float(x2)
+    y2 = float(y2)
+
+    plusX = (x1+x2)
+    plusY = (y1+y2)
+  
+    længdeAfplus = math.sqrt(plusY**2+plusX**2)
+    print(længdeAfplus)
+
+def Vektor2Dvinkel(x1,x2,y1,y2):
+    vinkel = atan(y1/x1)
+    vinke2 = atan(y2/x2)
+
+def Vektor2Denhedsvektor(x1,x2,y1,y2):
+    enhedsvektor1 
+
+def krydsvektor3D (x1,x2,y1,y2,z1,z2):
+    Xverdien = (y1*z2-z1*y2)
+    Yverdien = (z1*x2-x1*z2)
+    Zverdien = (x1*y2-y1*x2)
+
+def stedvektor ():
+
+def():
+
+def():
+
+def():
+
+def():
+
+def():
+
+def():
+
+def():
+
+def():
+
+def():
+
+def():
+
+def():
+
+def():
+
+def():
+
+    
+    
